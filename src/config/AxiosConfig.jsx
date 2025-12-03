fddbb452378af9c17e8d303cf79666b6335cf7f7
Mikael Gaviria <124961270+MikaelGG@users.noreply.github.com>
2025-12-03 14:40:15 -0500

    Change Axios base URL to production server

--- src/config/AxiosConfig.jsx
+++ src/config/AxiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://54.94.49.112:8080',
     headers: {
         'Content-Type': 'application/json'
     }
